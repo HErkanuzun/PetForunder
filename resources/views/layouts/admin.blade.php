ccca1859510ceaa44634f518bcac704fefb3c4df
ERKAN UZUN <erkanuzun@ERKANs-MacBook-Pro.local>
2023-05-30 14:43:47 +0300

    fc

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,13 @@
+<html>
+<!--Head-->
+@include("admin._head")
+</html>
+<body>
+<!--Content-->
+@section("content")
+@show
+<!--Footer-->
+@include("admin._footer")
+<!--Scripts-->
+@include("admin._scripts")
+</body>
